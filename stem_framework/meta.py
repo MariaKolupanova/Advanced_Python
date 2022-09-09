ae0759cdaa6b47d85d601b6734a42991d704e5b4
MariaKolupanova <109056436+MariaKolupanova@users.noreply.github.com>
2022-09-10 01:38:42 +0300

    Update meta.py

--- stem_framework/meta.py
+++ stem_framework/meta.py
@@ -1,1 +1,6 @@
-metadata (user-defined tree of values) are allowed to be used as input, meaning no user instructions (scripts) or manually managed intermediate states are possible. This means that user is forced to use declarative approach to describe what should be done instead of imperative one. One can automatically transform metadata, use it to cache analysis results or even send instructions to remote computing node. All these features are hard to implement using scripting approach. 
+# metadata (user-defined tree of values) are allowed to be used as input, 
+#meaning no user instructions (scripts) or manually managed intermediate states 
+#are possible. This means that user is forced to use declarative approach to describe
+#what should be done instead of imperative one. One can automatically transform 
+#metadata, use it to cache analysis results or even send instructions to remote 
+#computing node. All these features are hard to implement using scripting approach. 
